608038c95f398809151338ddaf1fcf3277b97450
Thomas <evers.thomas@gmail.com>
2021-03-19 13:02:53 +0100

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -21,3 +21,26 @@
   <body>
     <div id="page" class="site">
       
+
+    <a class="skip-link screen-reader-text" href="#content">skip to content</a>
+    <header role="banner">
+      <div class="wrapper">
+        <h1><a href="/">MyHomepage</a></h1>
+        <p>Home Header</p>
+        
+        <nav role="navigation" id="site-navigation" class="main-navigation">
+          <button class="menu-toggle hamburger hamburger--spin" aria-controls="primary-menu" aria-expanded="false">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+          <div class="wrapper">
+            <ul>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/uber-uns/'>Über uns</a></li>
+              <li id="no-ulul"><a href='/weblog/'>Weblog</a></li>
+            </ul>
+          </div>
+        </nav><!-- #site-navigation -->
+      </div>
+    </header>
