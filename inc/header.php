89917859db92a02cf89aa760dbd0e49c7a9c23dc
Thomas <evers.thomas@gmail.com>
2021-03-19 13:02:13 +0100

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="de"> <!-- Set this to the main language of your site -->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+    <title>MyHomepage</title>
+    
+    <!-- Enter a brief description of your page -->
+    <meta name="author" content="Thomas Evers">
+    <meta name="description" content="ein processwire starter template mit sass und einer einfachen blog funktion">
+    <meta name="keywords" content="processwire, template, sass, weblog, thomas evers">
+    
+    <!-- Include the site stylesheet -->
+    <link rel="stylesheet" type="text/css" href="/site/templates/styles/style.css">
+    
+    <!-- Include the HTML5 shiv print polyfill for Internet Explorer browsers 8 and below -->
+    <!--[if lt IE 9]><script src="js/html5shiv-printshiv.js" media="all"></script><![endif]-->
+  </head>
+  
+  <body>
+    <div id="page" class="site">
+      
