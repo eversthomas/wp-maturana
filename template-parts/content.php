871934f57821f4f91055e78dbfd1d7ab375b04c8
Thomas <evers.thomas@gmail.com>
2021-03-19 13:18:26 +0100

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,1 +1,12 @@
 
+<?php
+
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'wp_maturana' );
+endif;
+
+?>
