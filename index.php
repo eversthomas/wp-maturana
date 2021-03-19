da87d495cd6378728179ed3cbf01e680bc60da65
Thomas <evers.thomas@gmail.com>
2021-03-19 13:00:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-
+<!DOCTYPE html>
+<html lang="de"> <!-- Set this to the main language of your site -->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+    <title>MyHomepage</title>
+    
+    <!-- Enter a brief description of your page -->
+    <meta name="author" content="Thomas Evers">
+    <meta name="description" content="ein processwire starter template mit sass und einer einfachen blog funktion">
+    <meta name="keywords" content="processwire, template, sass, weblog, thomas evers">
+    
+    <!-- Include the site stylesheet -->
+    <link rel="stylesheet" type="text/css" href="/site/templates/styles/style.css">
+    
+    <!-- Include the HTML5 shiv print polyfill for Internet Explorer browsers 8 and below -->
+    <!--[if lt IE 9]><script src="js/html5shiv-printshiv.js" media="all"></script><![endif]-->
+  </head>
+  
+  <body><div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content">skip to content</a>
+    <header role="banner">
+      <div class="wrapper">
+        <h1><a href="/">MyHomepage</a></h1>
+        <p>Home Header</p>
+        
+        <nav role="navigation" id="site-navigation" class="main-navigation">
+          <button class="menu-toggle hamburger hamburger--spin" aria-controls="primary-menu" aria-expanded="false">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+          <div class="wrapper">
+            <ul>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/uber-uns/'>Über uns</a></li>
+              <li id="no-ulul"><a href='/weblog/'>Weblog</a></li>
+            </ul>
+          </div>
+        </nav><!-- #site-navigation -->
+      </div>
+    </header>
+    
+    <div id="content" class="wrapper with-sidebar">
+      <main role="main">
+        <article>
+        </article>
+      </main>
+      
+      <aside role="complementary">
+      </aside>
+    </div><!-- class page -->
+    
+    <footer role="contentinfo">
+      <div class="wrapper">
+      </div>
+    </footer>
+    
+    <!-- scripts -->
+    
+    </div><!-- id page -->
+  </body>
+</html>
