57cad23990a4569ef66ddfefd6142d29191977d2
Thomas <evers.thomas@gmail.com>
2021-03-23 13:32:48 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
+<?php
 /**
- * MyFirstTheme's functions and definitions
+ * wp_maturana's functions and definitions
  *
- * @package MyFirstTheme
- * @since MyFirstTheme 1.0
+ * @package wp_maturana
+ * @since wp_maturana 1.0
  */
  
 /**
@@ -12,7 +13,7 @@
 if ( ! isset( $content_width ) )
     $content_width = 800; /* pixels */
  
-if ( ! function_exists( 'myfirsttheme_setup' ) ) :
+if ( ! function_exists( 'wp_maturana_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -20,13 +21,13 @@ if ( ! function_exists( 'myfirsttheme_setup' ) ) :
  * before the init hook. The init hook is too late for some features, such as indicating
  * support post thumbnails.
  */
-function myfirsttheme_setup() {
+function wp_maturana_setup() {
  
     /**
      * Make theme available for translation.
      * Translations can be placed in the /languages/ directory.
      */
-    load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
+    load_theme_textdomain( 'wp_maturana', get_template_directory() . '/languages' );
  
     /**
      * Add default posts and comments RSS feed links to <head>.
@@ -42,8 +43,8 @@ function myfirsttheme_setup() {
      * Add support for two custom navigation menus.
      */
     register_nav_menus( array(
-        'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
-        'secondary' => __('Secondary Menu', 'myfirsttheme' )
+        'primary'   => __( 'Primary Menu', 'wp_maturana' ),
+        'secondary' => __('Secondary Menu', 'wp_maturana' )
     ) );
  
     /**
@@ -52,5 +53,41 @@ function myfirsttheme_setup() {
      */
     add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
 }
-endif; // myfirsttheme_setup
-add_action( 'after_setup_theme', 'myfirsttheme_setup' );
+endif; // wp_maturana_setup
+add_action( 'after_setup_theme', 'wp_maturana_setup' );
+
+/**
+ * styles and scripts
+ */
+function add_theme_scripts() {
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+ 
+  wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all');
+ 
+  wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+ 
+    if ( is_singular() &amp;amp;amp;&amp;amp;amp; comments_open() &amp;amp;amp;&amp;amp;amp; get_option( 'thread_comments' ) ) {
+      wp_enqueue_script( 'comment-reply' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+/**
+ * sidebars and widgets
+ */
+add_action( 'widgets_init', 'wp_maturana_sidebars' );
+function wp_maturana_sidebars() {
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'primary',
+            'name'          => __( 'Primary Sidebar' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+    /* Repeat register_sidebar() code for additional sidebars. */
+}
